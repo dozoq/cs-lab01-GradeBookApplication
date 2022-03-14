f75294c71177356ddbb425e59b2ae5e42c5e22fd
sebkewmeszke <sebkewmeszke@gmail.com>
2022-03-14 13:51:49 +0100

    Override CalculateStatistics For RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,6 +11,18 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
             char grade = 'F';
